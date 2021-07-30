d86ca1c3945a28595227ad1de587428992158109
Joao Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-07-30 11:27:50 -0300

    create auth flow in Google account

--- main.py
+++ main.py
@@ -1,24 +1,59 @@
 from selenium import webdriver
 import time
 
-from dotenv import *
+from keys import var
 
-options = webdriver.ChromeOptions()
-options.binary_location = BRAVE_PATH
 
-navegador = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
+def main():
+    def getEmail():
+        email = str(input("Digite o seu email para fazer login no YouTube: "))
+        return email
 
-email = str(input("Digite o seu email para fazer login no YouTube: "))
-senha = str(input("Digite a senha para o respectivo email: "))
+    def getPassword():
+        senha = str(input("Digite a senha para o respectivo email: "))
+        return senha
 
-navegador.get("https://accounts.google.com/signin/v2/identifier?service=youtube")
+    options = webdriver.ChromeOptions()
+    options.binary_location = var.BRAVE_PATH
 
-navegador\
-    .find_element_by_xpath('//*[@id="identifierId"]')\
-    .send_keys("{}".format(email))
+    navegador = webdriver.Chrome(
+        executable_path=var.DRIVER_PATH, options=options)
 
-time.sleep(1)
+    # email = getEmail()
+    # senha = getPassword()
 
-navegador\
-    .find_element_by_xpath('//*[@id="identifierNext"]/div/button')\
-    .click()
+    email = var.EMAIL_ADDRES
+    senha = var.EMAIL_PASSWORD
+
+    navegador.get(
+        "https://accounts.google.com/signin/v2/identifier?service=youtube")
+
+    navegador\
+        .find_element_by_xpath('//*[@id="identifierId"]')\
+        .send_keys("{}".format(email))
+
+    time.sleep(0.5)
+
+    navegador\
+        .find_element_by_xpath('//*[@id="identifierNext"]/div/button')\
+        .click()
+
+    time.sleep(2)
+
+    navegador\
+        .find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')\
+        .send_keys("{}".format(senha))
+
+    time.sleep(0.5)
+
+    navegador\
+        .find_element_by_xpath('//*[@id="passwordNext"]/div/button/span')\
+        .click()
+
+    time.sleep(2)
+
+    navegador.get('https://youtube.com')
+
+
+if __name__ == "__main__":
+    main()
